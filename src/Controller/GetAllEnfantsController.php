e9157e987c2de43c70a6b9f18da47c5fc80650c4
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-17 14:06:36 +0100

    Get all enfants

---
+++ src/Controller/GetAllEnfantsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Enfant;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class GetAllEnfantsController extends AbstractController
+{
+    #[Route('/api/get_all_enfants', name: 'api_get_all_enfants',  methods: ['GET'])]
+    public function listeEnfants(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $enfants = $entityManager->getRepository(Enfant::class)->createQueryBuilder('e')
+            ->select('e.NPI_enfant, e.Nom_enfant, e.Prenom_enfant, e.Classe_actuelle')
+            ->getQuery()
+            ->getResult();
+
+        return $this->json($enfants);
+    }
+}
