1e4b3f9d0d308a59634d3c8cb77d55d0fda4f79e
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-23 18:01:14 +0100

    educteur

--- src/Controller/RegisterEducateurController.php
+++ src/Controller/RegisterEducateurController.php
@@ -64,6 +64,7 @@ final class RegisterEducateurController extends AbstractController
          $educateur = new Educateur();
          $educateur->setNPI($data['NPI']);
          $educateur->setDateNaissance(new \DateTime());
+         $educateur->setEtoiles(2.5);
  
          $entityManager->persist($educateur);
          $entityManager->flush();
