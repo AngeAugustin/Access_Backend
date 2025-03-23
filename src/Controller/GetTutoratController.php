7bf3ddbc281fee95529ac8f0bd83b4015d51b4ce
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-23 19:32:32 +0100

    d

--- src/Controller/GetTutoratController.php
+++ src/Controller/GetTutoratController.php
@@ -2,23 +2,27 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+
 use App\Entity\Tutorat;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
  
 final class GetTutoratController extends AbstractController
 {
-    #[Route('/api/get_tutorat', name: 'api_get_tutorat', methods: ['GET'])]
-    public function listTutorat(EntityManagerInterface $entityManager): JsonResponse
+    #[Route('/api/get_tutorat/{NPI_parent}', name: 'api_get_tutorat', methods: ['GET'])]
+    public function listTutorat(EntityManagerInterface $entityManager, Request $request, string $NPI_parent): JsonResponse
     {
-        // Récupération des tutorats avec jointure sur la table User
+        // Récupération des tutorats avec jointure sur la table User et filtre sur le NPI_parent
         $tutorats = $entityManager->getRepository(Tutorat::class)
             ->createQueryBuilder('t')
             ->select('t.NPI_educateur, u.Name, u.Firstname, u.Matiere')
             ->leftJoin(User::class, 'u', 'WITH', 'u.NPI = t.NPI_educateur')
+            ->where('t.NPI_parent = :npiParent')
+            ->setParameter('npiParent', $NPI_parent)
             ->getQuery()
             ->getResult();
 
