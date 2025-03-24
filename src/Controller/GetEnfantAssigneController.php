4e9e2adc19cf353006251b19599b2ea35b1aad59
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-24 22:10:18 +0100

    24 Mars

--- src/Controller/GetEnfantAssigneController.php
+++ src/Controller/GetEnfantAssigneController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Tutorat;
+use App\Entity\Enfant;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class GetEnfantAssigneController extends AbstractController
+final class GetTutoratController extends AbstractController
 {
-    #[Route('/get/enfant/assigne', name: 'app_get_enfant_assigne')]
-    public function index(): Response
+    #[Route('/api/get_assignes', name: 'api_get_assignes', methods: ['GET'])]
+    public function getTutorats(EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('get_enfant_assigne/index.html.twig', [
-            'controller_name' => 'GetEnfantAssigneController',
-        ]);
+        // Création de la requête pour récupérer tous les assignes
+        $query = $entityManager->getRepository(Tutorat::class)->createQueryBuilder('t')
+            ->select('t.Reference_tutorat, e.Nom_enfant, e.Prenom_enfant, u.Matiere')
+            ->leftJoin(Enfant::class, 'e', 'WITH', 'e.NPI = t.NPI_enfant') 
+            ->leftJoin(User::class, 'u', 'WITH', 'u.NPI = t.NPI_educateur') 
+            ->getQuery();
+
+        // Exécuter la requête pour récupérer tous les tutorats
+        $tutorats = $query->getResult();
+
+        // Vérifier si des tutorats ont été trouvés
+        if (empty($tutorats)) {
+            return $this->json(['message' => 'Aucun tutorat trouvé'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        // Retourner les tutorats au format JSON
+        return $this->json($tutorats, JsonResponse::HTTP_OK);
     }
 }
