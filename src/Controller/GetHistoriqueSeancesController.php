00935add38e5ecd6586152f0682abc46bce8ffae
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-26 16:14:46 +0100

    26 Mars

--- src/Controller/GetHistoriqueSeancesController.php
+++ src/Controller/GetHistoriqueSeancesController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Seance;
+use App\Entity\Enfant;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class GetHistoriqueSeancesController extends AbstractController
 {
-    #[Route('/get/historique/seances', name: 'app_get_historique_seances')]
-    public function index(): Response
+    #[Route('/api/get_historique_seance/{Reference_tutorat}', name: 'api_get_historique', methods: ['GET'])]
+    public function getHistoriqueSeances(string $Reference_tutorat, EntityManagerInterface $entityManager): JsonResponse
     {
-        return $this->render('get_historique_seances/index.html.twig', [
-            'controller_name' => 'GetHistoriqueSeancesController',
-        ]);
+        // Création de la requête pour récupérer l'historique des séances
+        $query = $entityManager->getRepository(Seance::class)->createQueryBuilder('s')
+            ->select('s.Id_seance', 's.Date_seance', 's.Heure_seance', 's.Observation', 's.Travail_effectue')
+            ->where('s.Reference_tutorat = :Reference_tutorat')
+            ->setParameter('Reference_tutorat', $Reference_tutorat)
+            ->getQuery();
+
+        // Exécuter la requête pour récupérer tout l'historique
+        $historique = $query->getResult();
+
+        // Vérifier si l'historique a été trouvée
+        if (empty($historique)) {
+            return $this->json([
+                'message' => 'Aucun historique trouvé pour cette relation de tutorat ' . $Reference_tutorat,
+                'status' => JsonResponse::HTTP_NOT_FOUND
+            ], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        // Retourner les détails au format JSON
+        return $this->json([
+            'status' => JsonResponse::HTTP_OK,
+            'data' => $historique
+        ], JsonResponse::HTTP_OK);
     }
 }
