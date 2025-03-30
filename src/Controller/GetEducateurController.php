48aa2266d181a2ecc7dd3454e8855d4e354c6140
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-30 21:30:06 +0100

    30 Mars

--- src/Controller/GetEducateurController.php
+++ src/Controller/GetEducateurController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\Educateur;
 use App\Entity\Tutorat;
+use App\Entity\Enfant; // Assurez-vous d'importer la classe Enfant
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,9 +18,11 @@ final class GetEducateurController extends AbstractController
     {
          // Création de la requête pour récupérer les données
          $query = $entityManager->getRepository(User::class)->createQueryBuilder('u')
-         ->select('u.NPI', 'u.Name', 'u.Firstname', 'u.Email', 'u.Adresse', 'u.Matiere', 'd.Experience', 'd.Parcours', 't.NPI_enfant', 't.Duree_tutorat')
+         ->select('u.NPI', 'u.Name', 'u.Firstname', 'u.Email', 'u.Adresse', 'u.Matiere', 'd.Experience', 'd.Parcours', 
+                  't.NPI_enfant', 't.Duree_tutorat', 'e.Nom_enfant', 'e.Prenom_enfant', 'e.Classe_actuelle')
          ->leftJoin(Educateur::class, 'd', 'WITH', 'd.NPI = u.NPI')
          ->leftJoin(Tutorat::class, 't', 'WITH', 't.NPI_educateur = u.NPI')
+         ->leftJoin(Enfant::class, 'e', 'WITH', 'e.NPI_enfant = t.NPI_enfant')  // Jointure avec la table Enfant
          ->where('u.NPI = :NPI')
          ->setParameter('NPI', $NPI)
          ->getQuery();
@@ -50,14 +53,16 @@ final class GetEducateurController extends AbstractController
          ];
      }
 
-     // Initialiser un tableau pour les données NPI_enfant et Duree_tutorat
+     // Initialiser un tableau pour les données NPI_enfant, Nom_enfant, Prenom_enfant et Classe_actuelle
      $tutoratDetails = [];
 
-     // Extraire les données NPI_enfant et Duree_tutorat dans un tableau distinct
+     // Extraire les données NPI_enfant, Nom_enfant, Prenom_enfant, Classe_actuelle et Duree_tutorat
      foreach ($details as $detail) {
-         if (isset($detail['NPI_enfant'], $detail['Duree_tutorat'])) {
+         if (isset($detail['Nom_enfant'], $detail['Prenom_enfant'], $detail['Classe_actuelle'], $detail['Duree_tutorat'])) {
              $tutoratDetails[] = [
-                 'NPI_enfant' => $detail['NPI_enfant'],
+                 'Nom_enfant' => $detail['Nom_enfant'],
+                 'Prenom_enfant' => $detail['Prenom_enfant'],
+                 'Classe_actuelle' => $detail['Classe_actuelle'],
                  'Duree_tutorat' => $detail['Duree_tutorat']
              ];
          }
@@ -68,9 +73,8 @@ final class GetEducateurController extends AbstractController
          'status' => JsonResponse::HTTP_OK,
          'data' => [
              'educateur' => array_values($educateurDetails), // Supprimer les doublons
-             'tutorat' => $tutoratDetails // Tableau des données NPI_enfant et Duree_tutorat
+             'tutorat' => $tutoratDetails // Tableau des données Nom_enfant, Prenom_enfant, Classe_actuelle et Duree_tutorat
          ]
      ], JsonResponse::HTTP_OK);
  }
 }
-
