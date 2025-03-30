dfc43d5994622fa6a125f9f45fd8f53a1e987650
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-30 20:49:51 +0100

    30 Mars

--- src/Controller/GetEducateurController.php
+++ src/Controller/GetEducateurController.php
@@ -15,25 +15,30 @@ final class GetEducateurController extends AbstractController
     #[Route('/api/get_educateur/{NPI}', name: 'api_get_educateur', methods: ['GET'])]
     public function getEducateur(string $NPI, EntityManagerInterface $entityManager): JsonResponse
     {
-        // Récupération de l'éducateur par son NPI
-        $query = $entityManager->getRepository(User::class)->createQueryBuilder('u')
-            ->select('u.NPI, u.Name, u.Firstname, u.Matiere, u.Email, u.Telephone, u.Statut_profil, u.Adresse, e.Situation_matrimoniale, e.Experience, e.Parcours, e.Etoiles, t.NPI_enfant, t.Duree_tutorat')
-            ->leftJoin(Educateur::class, 'e', 'WITH', 'e.NPI = u.NPI')
-            ->leftJoin(Tutorat::class, 't', 'WITH', 't.NPI_educateur = u.NPI')
-            ->where('u.Role = :role')
-            ->andWhere('u.NPI = :NPI')
-            ->setParameter('role', 'EDUCATEUR')
-            ->setParameter('NPI', $NPI)
-            ->getQuery();
+         // Création de la requête pour récupérer t
+         $query = $entityManager->getRepository(User::class)->createQueryBuilder('u')
+         ->select('u.NPI', 'u.Name', 'u.Firstname', 'u.Email', 'u.Adresse', 'u.Matiere', 'd.Experience', 'd.Parcours', 't.NPI_enfant', 't.Duree_tutorat')
+         ->leftJoin(Educateur::class, 'd', 'WITH', 'd.NPI = u.NPI')
+         ->leftJoin(Tutorat::class, 't', 'WITH', 't.NPI_educateur = u.NPI')
+         ->where('u.NPI = :NPI')
+         ->setParameter('NPI', $NPI)
+         ->getQuery();
 
-        $educateur = $query->getOneOrNullResult();
+     // Exécuter la requête pour récupérer 
+     $details = $query->getResult();
 
-        // Vérifier si un éducateur a été trouvé
-        if (!$educateur) {
-            return $this->json(['message' => 'Éducateur non trouvé'], JsonResponse::HTTP_NOT_FOUND);
-        }
+     // Vérifier si des détails ont été trouvés
+     if (empty($details)) {
+         return $this->json([
+             'message' => 'Aucun détail trouvé pour éducateur avec NPI ' . $NPI,
+             'status' => JsonResponse::HTTP_NOT_FOUND
+         ], JsonResponse::HTTP_NOT_FOUND);
+     }
 
-        // Retourner les données au format JSON
-        return $this->json($educateur, JsonResponse::HTTP_OK);
-    }
+     // Retourner les détails au format JSON
+     return $this->json([
+         'status' => JsonResponse::HTTP_OK,
+         'data' => $details
+     ], JsonResponse::HTTP_OK);
+ }
 }
