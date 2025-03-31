f51532249d4721a738906a648c5ea83c79f24a98
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-31 20:14:17 +0100

    31 Mars

--- src/Controller/GetProfilDetailsController.php
+++ src/Controller/GetProfilDetailsController.php
@@ -9,31 +9,26 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 final class GetProfilDetailsController extends AbstractController
 {
     #[Route('/api/get_profil_details/{NPI}', name: 'api_get_profil_details', methods: ['GET'])]
     public function getProfilDetails(string $NPI, EntityManagerInterface $entityManager): JsonResponse
     {
-        $profil = $entityManager->getRepository(User::class)->findOneBy(['NPI' => $NPI]);
+        $query = $entityManager->createQuery(
+            'SELECT u.NPI, u.Name, u.Firstname, u.Email, u.Adresse, u.Telephone, 
+                    u.Situation_matrimoniale, u.Statut_profil, 
+                    e.Garant_1, e.Garant_2
+            FROM App\Entity\User u
+            LEFT JOIN App\Entity\Educateur e WITH u.NPI = e.NPI
+            WHERE u.NPI = :NPI'
+        )->setParameter('NPI', $NPI);
+
+        $profil = $query->getOneOrNullResult();
 
         if (!$profil) {
             return new JsonResponse(['error' => 'Aucun profil trouvé avec ce NPI'], Response::HTTP_NOT_FOUND);
         }
 
-        // Préparer les données 
-        $data = [ 
-            'NPI' => $profil->getNPI(),
-            'Name' => $profil->getName(),
-            'Firstname' => $profil->getFirstname(),
-            'Email' => $profil->getEmail(),
-            'Adresse' => $profil->getAdresse(),
-            'Telephone' => $profil->getTelephone(),
-            'Situation_matrimoniale' => $profil->getSituationMatrimoniale(),
-            'Statut_profil' => $profil->getStatutProfil(),
-        ];
-
-        return new JsonResponse($data, Response::HTTP_OK);
+        return new JsonResponse($profil, Response::HTTP_OK);
     }
-} 
- 
+}
