07befa3d7e25c3196b5112e112c02cb4e1bf3447
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-03-31 10:37:42 +0100

    31 Mars

--- src/Controller/GetAllSeancesController.php
+++ src/Controller/GetAllSeancesController.php
@@ -3,25 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Seance;
+use App\Entity\Enfant;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 final class GetAllSeancesController extends AbstractController
 {
     #[Route('/api/get_all_seances', name: 'api_get_all_seances', methods: ['GET'])]
     public function getAllSeances(EntityManagerInterface $entityManager): JsonResponse
     {
-        // Récupération des seances avec une validation de la requête
-        $seances = $entityManager->getRepository(Seance::class)->createQueryBuilder('s')
-            ->select('s.Id_seance, s.NPI_educateur, s.NPI_enfant, s.Heure_seance')
+        // Récupération des séances avec jointures
+        $seances = $entityManager->getRepository(Seance::class)
+            ->createQueryBuilder('s')
+            ->select(
+                's.Id_seance',
+                'e.Nom_enfant AS NomEnfant',
+                'e.Prenom_enfant AS PrenomEnfant',
+                'u.Name AS NomEducateur',
+                'u.Firstname AS PrenomEducateur',
+                's.Heure_seance'
+            )
+            ->leftJoin(Enfant::class, 'e', 'WITH', 's.NPI_enfant = e.NPI_enfant')
+            ->leftJoin(User::class, 'u', 'WITH', 's.NPI_educateur = u.NPI')
             ->getQuery()
             ->getResult();
 
-        // Si aucune séance n'est trouvé
+        // Vérification si aucune séance n'est trouvée
         if (empty($seances)) {
-            return $this->json(['message' => 'Aucun parent trouvé'], JsonResponse::HTTP_NOT_FOUND);
+            return $this->json(['message' => 'Aucune séance trouvée'], JsonResponse::HTTP_NOT_FOUND);
         }
 
         // Retourner les séances sous format JSON
