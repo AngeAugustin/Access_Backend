4faf32e4a0fb71ba44a2c45be9d2b53466941892
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-05-12 10:11:58 +0100

    Disponibilotés fixing

--- src/Controller/GetDispoController.php
+++ src/Controller/GetDispoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Educateur;
+use App\Entity\Tutorat;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,18 +15,49 @@ final class GetDispoController extends AbstractController
     #[Route('/api/get_dispo/{NPI}', name: 'api_get_dispo', methods: ['GET'])]
     public function getDispo(string $NPI, EntityManagerInterface $entityManager): JsonResponse
     {
-        $dispo = $entityManager->getRepository(Educateur::class)->findOneBy(['NPI' => $NPI]);
+        // Récupération de l'éducateur avec ses disponibilités
+        $educateur = $entityManager->getRepository(Educateur::class)->findOneBy(['NPI' => $NPI]);
 
-        if (!$dispo) {
-            return new JsonResponse(['error' => 'Aucun disponibilité récupérée avec ce NPI'], Response::HTTP_NOT_FOUND);
+        if (!$educateur) {
+            return new JsonResponse(['error' => 'Aucun éducateur trouvé avec ce NPI'], Response::HTTP_NOT_FOUND);
         }
 
-        $data = [ 
-            'NPI' => $dispo->getNPI(),
-            'Dispo1' => $dispo->getDispo1(),
-            'Dispo2' => $dispo->getDispo2(),
-            'Dispo3' => $dispo->getDispo3(),
-            'Dispo4' => $dispo->getDispo4(),
+        // Récupération des séances de tutorat associées à cet éducateur
+        $tutorats = $entityManager->getRepository(Tutorat::class)->findBy(['NPI_educateur' => $NPI]);
+
+        // Récupération de toutes les disponibilités de l'éducateur
+        $disponibilites = [
+            'Dispo1' => $educateur->getDispo1(),
+            'Dispo2' => $educateur->getDispo2(),
+            'Dispo3' => $educateur->getDispo3(),
+            'Dispo4' => $educateur->getDispo4(),
+        ];
+
+        // Création d'un tableau pour stocker les séances programmées
+        $seancesProgrammees = [];
+        
+        // Ajout des séances de tutorat au tableau des séances programmées
+        foreach ($tutorats as $tutorat) {
+            if ($tutorat->getSeance1()) {
+                $seancesProgrammees[] = $tutorat->getSeance1();
+            }
+            if ($tutorat->getSeance2()) {
+                $seancesProgrammees[] = $tutorat->getSeance2();
+            }
+        }
+
+        // Filtrage des disponibilités pour ne garder que celles qui ne sont pas des séances programmées
+        $disponibilitesLibres = [];
+        foreach ($disponibilites as $key => $dispo) {
+            if (!in_array($dispo, $seancesProgrammees) && $dispo !== null) {
+                $disponibilitesLibres[$key] = $dispo;
+            }
+        }
+
+        // Préparation des données pour la réponse
+        $data = [
+            'NPI' => $educateur->getNPI(),
+            'disponibilites_libres' => $disponibilitesLibres
         ];
 
         return new JsonResponse($data, Response::HTTP_OK);
