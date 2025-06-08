73fbcbc90f87122524773c67595d0e4c93e1f197
AngeAugustin <101171968+AngeAugustin@users.noreply.github.com>
2025-06-08 13:14:45 +0100

    ss

--- src/Controller/PayerParentController.php
+++ src/Controller/PayerParentController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PayerParentController extends AbstractController
 {
     #[Route('/api/payer_parent', name: 'payer_parent', methods: ['POST'])]
-    public function payerParent(Request $request, EntityManagerInterface $entityManager, \Psr\Log\LoggerInterface $logger): JsonResponse
+    public function payerParent(Request $request, EntityManagerInterface $entityManager, \Psr\Log\LoggerInterface $logger)
     {
         
         /* Adding webhook */ 
@@ -47,115 +47,78 @@ class PayerParentController extends AbstractController
             'transaction_id' => $transac_id
         ]);
 
-        die;
-
         // Handle the event
         switch ($event->name) {
             case 'transaction.created':
                 // Transaction créée
                 break;
             case 'transaction.approved':
-                
-                /* Handle true operation */
-                $campaign = $data->campaign_id;
-                $number = $data->number_of_votes;
-                $cost = $data->amount;
-                $infos = $data->phone_number;
 
-                $vote = 1;
+                $Id_paiement = $data->Id_paiement;
+                $Id_transaction = $transac_id;
+                $Paiement = $data->Paiement;
+                $NPI_payeur = $data->NPI_payeur;
+                $Nom_payeur = $data->Nom_payeur;
+                $Email_payeur = $data->Email_payeur;
+                $Prenom_payeur = $data->Prenom_payeur;
+                $Role_payeur = $data->Role_payeur;
+                $Montant_paiement = $data->Montant_paiement;
 
-                //Review
-                if(!$vote)
-                {
-                    if(!$vote)
-                    {
-                        echo json_encode(['status' => false, 'message' => 'Une erreur est intervenue']);
-                        http_response_code(200);
-                        exit;
-                    }else{
-                        echo json_encode(['status' => true, 'message' => 'Enregistrement effectif']);
-                        http_response_code(200);
-                        exit;
-                        
+                // Recherche du Paiement
+                $paiement = $entityManager->getRepository(Paiement::class)->find($Id_paiement);
+
+                if (!$paiement) {
+                    return new JsonResponse(['message' => 'Paiement non trouvé'], 404);
+                }
+
+                $updated = false;
+                $today = new \DateTime();
+
+                // Trouver la correspondance Paiement1, Paiement2 ou Paiement3
+                for ($i = 1; $i <= 3; $i++) {
+                    $getPaiement = 'getPaiement' . $i;
+                    $setStatut = 'setStatutPaiement' . $i;
+                    $setDate = 'setDatePaiement' . $i;
+
+                    if ($paiement->$getPaiement() === $Paiement) {
+                        $paiement->$setStatut('Provisoire');
+                        $paiement->$setDate($today);
+                        $updated = true;
+                        break;
                     }
                 }
+
+                if (!$updated) {
+                    return new JsonResponse(['message' => 'Paiement envoyé ne correspond pas à un Paiement enregistré'], 404);
+                }
+
+                $entityManager->persist($paiement);
+
+                // Création enregistrement PaiementParent
+                $paiementParent = new PaiementParent();
+                $paiementParent->setIdPaiement($Paiement);
+                $paiementParent->setIdTransaction($Id_transaction);
+                $paiementParent->setNPIPayeur($NPI_payeur);
+                $paiementParent->setNomPayeur($Nom_payeur);
+                $paiementParent->setPrenomPayeur($Prenom_payeur);
+                $paiementParent->setRolePayeur($Role_payeur);
+                $paiementParent->setEmailPayeur($Email_payeur);
+                $paiementParent->setStatutPaiement('Effectué');
+                $paiementParent->setDatePaiement($today->format('Y-m-d'));
+                $paiementParent->setMontantPaiement($Montant_paiement);
+
+                $entityManager->persist($paiementParent);
+                $entityManager->flush();
+
+                return new JsonResponse(['message' => 'Paiement parent enregistré avec succès']);
                 // Transaction approuvée
-                break;
-            case 'transaction.canceled':
-                // Transaction annulée
                 break;
             default:
                 http_response_code(400);
                 exit();
         }
 
-        http_response_code(200);
-    
-        
-        $data = json_decode($request->getContent(), true);
-
-        if (!$data
-            || !isset($data['Id_paiement'],$data['Id_transaction'], $data['Paiement'], $data['NPI_payeur'], $data['Nom_payeur'], $data['Prenom_payeur'], $data['Role_payeur'], $data['Montant_paiement'])) {
-            return new JsonResponse(['message' => 'Données manquantes ou invalides'], 400);
-        }
-
-        $Id_paiement = $data['Id_paiement'];
-        $Id_transaction = $data['Id_transaction'];
-        $Paiement = $data['Paiement'];
-        $NPI_payeur = $data['NPI_payeur'];
-        $Nom_payeur = $data['Nom_payeur'];
-        $Email_payeur = $data['Email_payeur'];
-        $Prenom_payeur = $data['Prenom_payeur'];
-        $Role_payeur = $data['Role_payeur'];
-        $Montant_paiement = $data['Montant_paiement'];
-
-        // Recherche du Paiement
-        $paiement = $entityManager->getRepository(Paiement::class)->find($Id_paiement);
-
-        if (!$paiement) {
-            return new JsonResponse(['message' => 'Paiement non trouvé'], 404);
-        }
-
-        $updated = false;
-        $today = new \DateTime();
-
-        // Trouver la correspondance Paiement1, Paiement2 ou Paiement3
-        for ($i = 1; $i <= 3; $i++) {
-            $getPaiement = 'getPaiement' . $i;
-            $setStatut = 'setStatutPaiement' . $i;
-            $setDate = 'setDatePaiement' . $i;
-
-            if ($paiement->$getPaiement() === $Paiement) {
-                $paiement->$setStatut('Provisoire');
-                $paiement->$setDate($today);
-                $updated = true;
-                break;
-            }
-        }
-
-        if (!$updated) {
-            return new JsonResponse(['message' => 'Paiement envoyé ne correspond pas à un Paiement enregistré'], 404);
-        }
-
-        $entityManager->persist($paiement);
-
-        // Création enregistrement PaiementParent
-        $paiementParent = new PaiementParent();
-        $paiementParent->setIdPaiement($Paiement);
-        $paiementParent->setIdTransaction($Id_transaction);
-        $paiementParent->setNPIPayeur($NPI_payeur);
-        $paiementParent->setNomPayeur($Nom_payeur);
-        $paiementParent->setPrenomPayeur($Prenom_payeur);
-        $paiementParent->setRolePayeur($Role_payeur);
-        $paiementParent->setEmailPayeur($Email_payeur);
-        $paiementParent->setStatutPaiement('Effectué');
-        $paiementParent->setDatePaiement($today->format('Y-m-d'));
-        $paiementParent->setMontantPaiement($Montant_paiement);
-
-        $entityManager->persist($paiementParent);
-        $entityManager->flush();
-
-        return new JsonResponse(['message' => 'Paiement parent enregistré avec succès']);
+        return new JsonResponse(['message' => 'Webhook reçu avec succès']);        
     }
 
 
